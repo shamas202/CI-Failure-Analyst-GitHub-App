1b7e904bf23b927786e7cc662a0f8af673ac220c
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:25:14 +0500

    Minor refactoring of function calls (refinement 18)

--- scripts/format-private-key.js
+++ scripts/format-private-key.js
@@ -74,3 +74,4 @@ async function main() {
 main().catch(console.error);
 # Refinement 12: Improving consistency across the module
 # Refinement 14: Minor refactoring of function calls
+# Refinement 18: Minor refactoring of function calls
