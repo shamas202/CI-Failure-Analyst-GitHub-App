388af2313f3cddb082f82a8d980581664f6da7bd
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:26:08 +0500

    Adding descriptive comments for better maintainability (refinement 109)

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,4 @@
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
 Refinement 106: Improving code documentation
+Refinement 109: Adding descriptive comments for better maintainability
